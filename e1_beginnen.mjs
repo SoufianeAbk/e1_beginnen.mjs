3bd02199c32c309247605cee63fc5ad2cadc7e86
SoufianeAbk <soufiane.abakkiou@student.ehb.be>
2024-09-24 17:40:24 +0200

    e1_beginnen.mjs

---
+++ e1_beginnen.mjs
@@ -0,0 +1,10 @@
+// Voeg hier je eigen code in
+let name;
+name = 'Soufiane';
+let age;
+age = 23;
+let favoriteGame;
+favoriteGame = 'FC24';
+
+
+console.log('Hallo '+ name +', ik hoor dat je '+ age +' jaar oud bent en dat je lievelingsgame '+ favoriteGame +' is.');
